a6b60e1068a447c742c30eca4fd6ae22f9e41baa
Mason James <mtj@kohaaloha.com>
2013-02-12 00:35:13 +1300

    modified:   blit-surface.pl 	new file:   sdl-color-cycle4.pl

--- blit-surface.pl
+++ blit-surface.pl
@@ -1,6 +1,5 @@
 use strict;
 use warnings;
-
 use SDL;
 use SDL::Event;
 use SDL::Events;
@@ -15,12 +14,10 @@ use SDL::PixelFormat;
 use SDLx::Sprite;
 use SDL::GFX::Framerate;
 use SDL::GFX::FPSManager;
-
 my $DISPLAY_W = 200;
 my $DISPLAY_H = 200;
-
-my $DEBUG = 1;
-my $SCALE = 1;
+my $DEBUG     = 1;
+my $SCALE     = 1;
 
 # frames per second
 my $fps      = 60;
@@ -36,29 +33,24 @@ my $app = SDLx::App->new(
     title        => 'SDLx',
     resizeable   => 1,
     exit_on_quit => 1,
-    delay => 5
+    delay        => 5
 );
-
 my $canvas = SDLx::Surface->new(
     surface => SDL::Video::display_format(
         SDL::Surface->new( SDL_ANYFORMAT, $DISPLAY_W, $DISPLAY_H )
     )
 );
-
 $app->add_event_handler( \&get_keyboard_input );
 $app->add_show_handler( \&display );
 $app->run();
 
 #----------------------------------------------------------------
-
 sub get_keyboard_input {
     my ( $event, $app ) = @_;
-
     if ( $event->type == SDL_KEYDOWN ) {
         my $key_name = SDL::Events::get_key_name( $event->key_sym );
         $app->stop() if $key_name =~ /^q$/;
         $app->stop() if $key_name =~ /escape/;
-
         if ( $key_name eq 'tab' ) {
             $SCALE = $SCALE == 1 ? 3 : 1;
             $app->resize( $DISPLAY_W * $SCALE, $DISPLAY_H * $SCALE );
@@ -67,9 +59,7 @@ sub get_keyboard_input {
 }
 
 #----------------------------------------------------------------
-
 sub display {
-
     my $i = int( rand(255) ) + 1;
     my $color = SDL::Color->new( 255, 0, $i );
     $canvas->draw_rect( undef, $color );
@@ -80,14 +70,9 @@ sub display {
         $realFps = $fps / ( $t - $ticks ) * 1000;
         $ticks = $t;
     }
-
     $app->blit_by( SDL::GFX::Rotozoom::surface( $canvas, 0, $SCALE, undef ) );
-
     $realFps = sprintf( "%.1f", $realFps );
     $app->draw_gfx_text( [ 10, 10 ], 0xFFFFFFFF, "fps: $realFps" );
-
     $app->update;
     $frames++;
-
 }
-
---
+++ sdl-color-cycle4.pl
@@ -0,0 +1,87 @@
+use Modern::Perl;
+use SDL;
+use SDL::Video;
+use SDL::Surface;
+use SDL::Rect;
+use SDL::Color;
+use SDL::Palette;
+use SDL::GFX::Primitives;
+use Time::HiRes qw( usleep );
+use Data::Printer;
+
+# the size of the window box or the screen resolution if fullscreen
+my $screen_width  = 800;
+my $screen_height = 600;
+our $BROWNA = 0x973914FF;
+our $GREENA = 0x00FF00FF;
+SDL::init(SDL_INIT_VIDEO);
+
+# setting video mode
+my $screen_surface =
+  SDL::Video::set_video_mode( $screen_width, $screen_height, 8, SDL_HWPALETTE );
+
+#my $set_colors = SDL::Video::set_colors( $screen_surface, 0  , $BROWNA, $GREENA  );
+my $format = $screen_surface->format;
+my $pal    = $screen_surface->format->palette;
+my $btpp   = $screen_surface->format->BitsPerPixel;
+my $bypp   = $screen_surface->format->BytesPerPixel;
+my $color  = SDL::Color->new( 255, 0, 100 );
+
+#$canvas->draw_rect( undef, $color );
+my $format = $screen_surface->format;
+my $pal    = $screen_surface->format->palette;
+my $btpp   = $screen_surface->format->BitsPerPixel;
+my $bypp   = $screen_surface->format->BytesPerPixel;
+
+# my $aa = SDL::Palette::ncolors();
+#warn $aa ;
+warn '-------------------------';
+p $format;
+warn '-------------------------';
+p $pal;
+warn '-------------------------';
+p $btpp;
+warn '-------------------------';
+p $bypp;
+warn '-------------------------';
+warn $pal->ncolors;
+my @b = SDL::Palette::colors($pal);
+
+#file://localhost/Users/mason/git.wasabi/sdl/sdl.perl.org/SDL-Color.html
+#warn $pal->color_index($pal, 0);
+#p @b;
+#--------------------------------------------
+our $BLACKA  = 0x000000FF;
+our $REDA    = 0xFF0000FF;
+our $BROWNA  = 0x973914FF;
+our $GREENA  = 0x00FF00FF;
+our $YELLOWA = 0xeda829FF;
+our $BLUEA   = 0x141D94FF;
+our $PINKA   = 0xCC33FFFF;
+our $CYANA   = 0x71fafcFF;
+our $GREYA   = 0x818293FF;
+our $WHITEA  = 0xFFFFFFFF;
+
+#my $black = SDL::Color->new( 0x00, 0x00, 0x00 );
+#my $grey  =  SDL::Color->new( 0xFF, 0xFF, 0xFF );
+my $black = SDL::Color->new( 0x00, 0x00, 0x00 );
+my $grey  = SDL::Color->new( 255,  255,  255 );
+SDL::Video::set_colors( $screen_surface, 0, $black );
+SDL::Video::set_colors( $screen_surface, 0, $grey );
+
+#$screen_surface->draw_rect( undef, $black );
+#p $clr;
+my $i = 0;
+while (1) {
+    usleep(500000);
+    $i = $i == 0 ? 1 : 0;
+    warn $i;
+    my $clr = SDL::Palette::color_index( $pal, $i );
+    SDL::GFX::Primitives::box_color( $screen_surface, 1, 1, 100, 100, $clr );
+
+   #    SDL::GFX::Primitives::hline_color( $screen_surface, 2, 100 , 20 , $clr);
+   # update the whole screen
+    SDL::Video::fill_rect( $screen_surface, undef, $clr );
+    SDL::Video::update_rect( $screen_surface, 0, 0, $screen_width,
+        $screen_height );
+}
